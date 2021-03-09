c08891460a53344d77d493b935e83aff5327c6a8
AlmirPaulo <ap.freelas@gmail.com>
2021-03-09 16:43:16 -0300

    pre-deploy

--- main.py
+++ main.py
@@ -8,18 +8,22 @@ client = discord.Client()
 
 #Pick the card
 @client.event
-async def on_message(message):
+async def on_message(message):              
     if message.author == client.user:
       return
 
+   #Mention bot
+    if client.user.mentioned_in(message):
+      await message.channel.send('Hello!\n My prefix is *\nGood luck!')
+    
     if message.content == '*help':
       await message.channel.send('''
-      Prefix: *\n
       Website:https://almirpaulo.github.io/TarotMeditation\n
       Support: https://discord.gg/4sutReEVE8\n
-      Creator: https://github.com/AlmirPaulo\n
+      Author: https://github.com/AlmirPaulo\n
+      Technical Info: https://github.com/AlmirPaulo/TarotMeditation\n
       Vote for us: Soon...\n
-      Premium: Soon...''')
+      Premium: Soon...\n ''')
     
     
     if message.content == '*1card':
@@ -36,8 +40,6 @@ async def on_message(message):
       await message.channel.send('The Past, Present and Future.',files=[discord.File(f"cards/{str(pick[0])}.png"),
                                 discord.File(f"cards/{str(pick[1])}.png"),
                                 discord.File(f"cards/{str(pick[2])}.png")])  
-
-
 
     if message.content == '*2cards':
       cards = 2
