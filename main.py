bdb34847e73d02786bb3320ef214b3cbe84fdf2d
AlmirPaulo <ap.freelas@gmail.com>
2021-06-30 17:45:39 -0300

    new images

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ async def on_message(message):
     
     if message.content == '*1card':
       pick = random.randint(0,21)
-      await message.channel.send( 'An advice.',file=discord.File(f"cards/{str(pick)}.png"))
+      await message.channel.send( 'An advice.',file=discord.File(f"cards/{str(pick)}.jpg"))
 
     if message.content == '*3cards':
       cards = 3
@@ -37,9 +37,9 @@ async def on_message(message):
         pick.append(random.randint(0,21))
         cards = cards -1
         
-      await message.channel.send('The Past, Present and Future.',files=[discord.File(f"cards/{str(pick[0])}.png"),
-                                discord.File(f"cards/{str(pick[1])}.png"),
-                                discord.File(f"cards/{str(pick[2])}.png")])  
+      await message.channel.send('The Past, Present and Future.',files=[discord.File(f"cards/{str(pick[0])}.jpg"),
+                                discord.File(f"cards/{str(pick[1])}.jpg"),
+                                discord.File(f"cards/{str(pick[2])}.jpg")])  
 
     if message.content == '*2cards':
       cards = 2
@@ -48,12 +48,12 @@ async def on_message(message):
         pick.append(random.randint(0,21))
         cards = cards -1
         
-      await message.channel.send('The Positive and Negative sides.',files=[discord.File(f"cards/{str(pick[0])}.png"),
-                                discord.File(f"cards/{str(pick[1])}.png")])  
+      await message.channel.send('The Positive and Negative sides.',files=[discord.File(f"cards/{str(pick[0])}.jpg"),
+                                discord.File(f"cards/{str(pick[1])}.jpg")])  
 
     if message.content.startswith('*card'):
       num = message.content[6:8]
-      await message.channel.send(file=discord.File(f"cards/{num}.png"))
+      await message.channel.send(file=discord.File(f"cards/{num}.jpg"))
 
 
 #Run the bot
